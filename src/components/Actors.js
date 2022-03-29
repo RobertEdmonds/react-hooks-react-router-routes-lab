92631e639ef4445156d5d9e36bb67b1b8e193fe1
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-28 21:10:06 -0600

    Complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayActors = actors.map(item =>  {
+    const displayMovies = item.movies.map(movie => {
+      return <li key={movie}>{movie}</li>
+    })
+    return(
+      <div key={item.name}>
+        <h3>{item.name}</h3>
+        <ul>
+          {displayMovies}
+        </ul>
+      </div>
+    )
+  })
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {displayActors}
+  </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const displayMovies = movies.map(item => {
+    const displayGenre = item.genres.map(genre =>{
+      return <li key={genre}>{genre}</li>
+    })
+    return (
+    <div key={item.title}>
+      <h3>{item.title}</h3>
+      <h4>{item.time}</h4>
+      <ul>
+        {displayGenre}
+      </ul>
+    </div>)
+  })
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {displayMovies}
+    </div>
+  );
 }
 
 export default Movies;
